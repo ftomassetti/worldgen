af067add0407c6aa9cf8bb2311384913e51530e5
Federico Tomassetti <f.tomassetti@gmail.com>
2013-08-23 15:14:02 +0200

    Just generated with hoe

---
+++ lib/worldgen.rb
@@ -0,0 +1,3 @@
+class Worldgen
+  VERSION = "1.0.0"
+end
---
+++ test/test_worldgen.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require "worldgen"
+
+class TestWorldgen < MiniTest::Unit::TestCase
+  def test_sanity
+    flunk "write tests or I will kneecap you"
+  end
+end
